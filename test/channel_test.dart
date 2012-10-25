30330476104e912b89af53efb65feebb9d98fe9c
Justin Fagnani <justinfagnani@google.com>
2012-10-25 15:29:58 -0700

    Change package name to appengine_channel

--- test/channel_test.dart
+++ test/channel_test.dart
@@ -1,5 +1,5 @@
 import 'dart:html';
-import 'package:gae-channel-api-dart/channel.dart';
+import 'package:appengine_channel/channel.dart';
 import 'package:unittest/unittest.dart';
 import 'package:js/js.dart' as js;
 
